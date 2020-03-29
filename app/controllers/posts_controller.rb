09d3b2c409e54b231191650926414de4ee7f5c99
malekkbenhalima <malekbenhlima@icloud.com>
2020-03-29 19:49:23 +0200

    Add new, create, posts_params to posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,25 @@ class PostsController < ApplicationController
     def index  
     end
 
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(posts_params)
+
+        if @post.save
+            redirect_to(@post)
+        else
+            render 'new'
+        end
+
+    end
+
+    private
+
+    def posts_params
+        params.require(:post).permit(:title, :content)
+    end
+
 end
